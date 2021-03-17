d1684ce330d77957261bcfb3a6f59ab22ad2ec2a
Tran Le Hoang <lehoangtran289@gmail.com>
2021-03-17 12:59:27 +0700

    init lab02

---
+++ Lab02/exercise.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Receiving input</title>
+    </head>
+    <body>
+        <span style="font-size:large;color:blue">Thank you: Got your Input</span>
+        <?php
+            $name = $_POST["name"];
+            $class = $_POST["class"];
+            $university = $_POST["university"];
+            $hobbies = $_POST["hobbies"];
+            echo "<br>Hello, $name";
+            echo "<br>You are studying at $class, $university";
+            if (empty($hobbies)) {
+                echo "<br>No hobbies!!";
+            } else {
+                echo "<br>Your hobbies are:";
+                echo "<ol>";
+                foreach ($hobbies as &$hobby) {
+                    echo "<li>$hobby</li>";
+                }
+                echo "</ol>";
+            }
+        ?>
+    </body>
+</html>
