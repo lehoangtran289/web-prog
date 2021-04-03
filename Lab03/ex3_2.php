7483ea8b738686a528bcdfd7eb9b2e41a47da17c
dangvu99 <dangyeuzed@gmail.com>
2021-04-03 22:15:39 +0700

    fix ex3_2.php

--- Lab03/ex3_2.php
+++ Lab03/ex3_2.php
@@ -1,12 +1,12 @@
 <?php
 function reformat_str_date($date_str) {
-    list($d, $m, $y) = explode('/',$date_str);
+    list($d, $m, $y) = preg_split('/[-\/]/', $date_str);
     $new_str_date = $m . "/" . $d . "/" . $y;
     return $new_str_date;
 }
 
 function check_dob($dob) {
-    list($d, $m, $y) = explode('/',$dob);
+    list($d, $m, $y) = preg_split('/[-\/]/', $dob);
     if (!is_numeric($d) || !is_numeric($m) || !is_numeric($y))
         return 0;
     $day = intval($d);
@@ -19,21 +19,21 @@ function check_dob($dob) {
 
 function display_dob($dob) {
     $date_str = reformat_str_date($dob);
-    $date = strtotime($date_str);    
+    $date = strtotime($date_str);
     print "DOB: " . date("d, M, Y", $date);
 }
 
 function display_dob_diff($dob1, $dob2, $mode) {
     $date_str1 = reformat_str_date($dob1);
-    $date1 = date_create($date_str1);  
+    $date1 = date_create($date_str1);
     $date_str2 = reformat_str_date($dob2);
-    $date2 = date_create($date_str2);  
+    $date2 = date_create($date_str2);
     if ($mode == 1) {
         $interval = date_diff($date1, $date2);
         echo "<br>" . $interval -> format("Difference between two dobs is: %a day<br>");
     } else {
         $year_diff = $date1 -> diff($date2);
-        echo "Difference between two dobs in year is: " . ($year_diff -> m) / 12 . " years";
+        echo "Difference between two dobs in year is: " . ($year_diff -> y) . " years";
     }
 }
 
@@ -47,37 +47,48 @@ function cal_age($date) {
 ?>
 
 <html>
-    <head><title> Birthday </title></head>
-    <h1>Got your input</h1>
-    <body>
-    <?php
-        $name1 = $_POST["name1"];
-        $dob1 = $_POST["dob1"];
-        $name2 = $_POST["name2"];
-        $dob2 = $_POST["dob2"];
+<head><title> Birthday </title></head>
+<h1>Got your input</h1>
+<body>
+<?php
+if (!isset($_POST["name1"]) || $_POST['name1'] == '')
+    exit("Please enter name of the first person!<br>");
+if (!isset($_POST["dob1"]) || $_POST['dob1'] == '')
+    exit("Please enter dob of the first person!<br>");
+if (!isset($_POST["name2"]) || $_POST['name2'] == '')
+    exit("Please enter name of the second person!<br>");
+if (!isset($_POST["dob2"]) || $_POST['dob2'] == '')
+    exit("Please enter dob of the first person!<br>");
+$name1 = $_POST["name1"];
+$dob1 = $_POST["dob1"];
+echo "<br>";
+echo $dob1;
 
-        print "Name of the first person: " . $name1;
-        echo "<br>";
-        if (check_dob($dob1) == 0) {
-            print "DOB of the first person is invalid!!<br>";
-        } else display_dob($dob1);
-        echo "<br>";
+$name2 = $_POST["name2"];
+$dob2 = $_POST["dob2"];
 
-        print "Name of the second person: " . $name2;
-        echo "<br>";
-        if (check_dob($dob2) == 0) {
-            print "DOB of the second person is invalid!!<br>";
-        } else display_dob($dob2);
-        echo "<br>";
+print "Name of the first person: " . $name1;
+echo "<br>";
+if (check_dob($dob1) == 0) {
+    print "DOB of the first person is invalid!!<br>";
+} else display_dob($dob1);
+echo "<br>";
 
-        if (check_dob($dob1) && check_dob($dob2)) {
-            display_dob_diff($dob1, $dob2, 1);
-            $age1 = cal_age($dob1);
-            $age2 = cal_age($dob2);
-            print $name1 . " is " . $age1 . " years old.<br>";
-            print $name2 . " is " . $age2 . " years old.<br>";
-            display_dob_diff($dob1, $dob2, 2);
-        }
-    ?>
-    </body>
+print "Name of the second person: " . $name2;
+echo "<br>";
+if (check_dob($dob2) == 0) {
+    print "DOB of the second person is invalid!!<br>";
+} else display_dob($dob2);
+echo "<br>";
+
+if (check_dob($dob1) && check_dob($dob2)) {
+    display_dob_diff($dob1, $dob2, 1);
+    $age1 = cal_age($dob1);
+    $age2 = cal_age($dob2);
+    print $name1 . " is " . $age1 . " years old.<br>";
+    print $name2 . " is " . $age2 . " years old.<br>";
+    display_dob_diff($dob1, $dob2, 2);
+}
+?>
+</body>
 </html>
