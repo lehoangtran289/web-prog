86b56bb198628dcfa778bb653e16a817b116a2f8
Van Hoang <gmlvan99@gmail.com>
2021-04-03 21:43:50 +0700

    fix ex3_2

--- Lab03/ex3_2.php
+++ Lab03/ex3_2.php
@@ -1,12 +1,12 @@
 <?php
 function reformat_str_date($date_str) {
-    list($d, $m, $y) = explode('/',$date_str);
+    list($d, $m, $y) = preg_split('/[-\/]/', $date_str);
     $new_str_date = $m . "/" . $d . "/" . $y;
     return $new_str_date;
 }
 
 function check_dob($dob) {
-    list($d, $m, $y) = explode('/',$dob);
+    list($d, $m, $y) = preg_split('/[-\/]/', $dob);
     if (!is_numeric($d) || !is_numeric($m) || !is_numeric($y))
         return 0;
     $day = intval($d);
@@ -33,7 +33,7 @@ function display_dob_diff($dob1, $dob2, $mode) {
         echo "<br>" . $interval -> format("Difference between two dobs is: %a day<br>");
     } else {
         $year_diff = $date1 -> diff($date2);
-        echo "Difference between two dobs in year is: " . ($year_diff -> m) / 12 . " years";
+        echo "Difference between two dobs in year is: " . ($year_diff -> y) . " years";
     }
 }
 
@@ -51,6 +51,14 @@ function cal_age($date) {
     <h1>Got your input</h1>
     <body>
     <?php
+        if (!isset($_POST["name1"])) 
+            exit("Please enter name of the first person!<br>");
+        if (!isset($_POST["dob1"])) 
+            exit("Please enter dob of the first person!<br>");
+        if (!isset($_POST["name2"])) 
+            exit("Please enter name of the second person!<br>");
+        if (!isset($_POST["dob2"])) 
+            exit("Please enter dob of the first person!<br>");
         $name1 = $_POST["name1"];
         $dob1 = $_POST["dob1"];
         $name2 = $_POST["name2"];
