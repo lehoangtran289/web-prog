d507235d1c39a8e68dd542ad0115d63ebf683794
dangvu99 <dangyeuzed@gmail.com>
2021-04-14 14:05:27 +0700

    push lab7.2

---
+++ Lab05/exercise/exercise.php
@@ -0,0 +1,24 @@
+<?php
+//    class Page{
+//        private string $page;
+//        private string $title;
+//        private int $year;
+//        private string $copyRight;
+//
+//        private function addHeader(){
+//
+//        }
+//
+//        public function addContent(string $content){
+//
+//        }
+//
+//        private function addFooter(){
+//
+//        }
+//
+//        public function get(){
+//            return
+//        }
+//    }
+//?>
