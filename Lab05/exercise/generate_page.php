81f3a61f4728cf024fc70eacd96bc4bc4fe61ed2
dangvu99 <dangyeuzed@gmail.com>
2021-04-17 03:17:22 +0700

    add function addHeader()

--- Lab05/exercise/generate_page.php
+++ Lab05/exercise/generate_page.php
@@ -1,13 +1,10 @@
 <?php
-require_once "./page.php";
+include "./page.php";
 
 $title = $_POST["title"];
 $year = $_POST["year"];
 $copyright = $_POST["copyright"];
 $content = $_POST["content"];
-
-$page = NULL;
-
 
 $page = new Page($title, $year, $copyright);
 
