637fc5c3b555206a8b243456592dd2ea39bd752d
lehoangtran289 <hoangtl289@gmail.com>
2021-04-17 10:23:58 +0700

    rf

--- Lab06/6.3/Business.php
+++ Lab06/6.3/Business.php
@@ -109,11 +109,7 @@
 ?>
 
 <?php
-    $server_username = 'b387322b6006bc';
-    $server_password = '0739b344';
-    $server_host = "us-cdbr-east-03.cleardb.com";
-    $database = 'heroku_eedca10c2fc1c8a';
-    $conn = mysqli_connect($server_host, $server_username, $server_password, $database) or die("cant connect to database");
+    include '../common/connection.php';
     
     $categories = array();
     $bizcats = array();
