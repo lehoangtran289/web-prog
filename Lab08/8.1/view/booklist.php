f1a9128257ab7cd9f4243daa3902c07435054e2e
dangvu99 <dangyeuzed@gmail.com>
2021-05-12 15:58:32 +0700

    push lab8

--- Lab08/8.1/view/booklist.php
+++ Lab08/8.1/view/booklist.php
@@ -1,20 +1,19 @@
 <html>
-<head>Book List</head>
+<head></head>
+
 <body>
+
 <table>
-    <tbody>
-        <tr>
-            <td>Title</td>
-            <td>Author</td>
-            <td>Description</td>
-        </tr>
-    </tbody>
-    <?php
-        foreach($books as $title=>$book) {
-            echo '<tr><td><a href="../index.php?book=' . $book->title . '">' . $book->title . '</a></td><td>'
-                . $book->author . '</td><td>' . $book->description . '</td></tr>';
-        }
-    ?>
+	<tr><td>Title</td><td>Author</td><td>Description</td></tr>
+	<?php 
+
+		foreach ($books as $title => $book)
+		{
+			echo '<tr><td><a href="index.php?book='.$book->title.'">'.$book->title.'</a></td><td>'.$book->author.'</td><td>'.$book->description.'</td></tr>';
+		}
+
+	?>
 </table>
+
 </body>
 </html>
--- Lab08/8.1/view/viewbook.php
+++ Lab08/8.1/view/viewbook.php
@@ -1,10 +1,15 @@
 <html>
-<head>View Book</head>
+<head></head>
+
 <body>
-<?php
-    echo 'Title: ' . $book->title . '<br>';
-    echo 'Author: ' . $book->author . '<br>';
-    echo 'Description: ' . $book->description . '<br>';
+
+<?php 
+
+	echo 'Title:' . $book->title . '<br/>';
+	echo 'Author:' . $book->author . '<br/>';
+	echo 'Description:' . $book->description . '<br/>';
+
 ?>
+
 </body>
 </html>
