37afc770df00dfbce8ab7bcce922a8c55d6f2e7f
dangzed <dangvh99@gmail.com>
2021-05-16 20:21:07 +0700

    abc

--- Lab08/MVC-2/config/config.php
+++ Lab08/MVC-2/config/config.php
@@ -4,7 +4,7 @@
     
     const DEVELOPMENT_ENVIRONMENT = true;
     
-    const DB_NAME = 'todo';
-    const DB_USER = 'mysql_client';
-    const DB_PASSWORD = 'p';
-    const DB_HOST = 'db';
+    const DB_NAME = 'heroku_eedca10c2fc1c8a';
+    const DB_USER = 'b387322b6006bc';
+    const DB_PASSWORD = '0739b344';
+    const DB_HOST = 'us-cdbr-east-03.cleardb.com';
