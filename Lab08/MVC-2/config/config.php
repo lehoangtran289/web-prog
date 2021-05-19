39ebe9ce4ece66e396f0c43c066512567cf7ca7f
dangzed <dangvh99@gmail.com>
2021-05-19 13:06:55 +0700

    create directory for final project

--- Lab08/MVC-2/config/config.php
+++ Lab08/MVC-2/config/config.php
@@ -3,9 +3,8 @@
     /** Configuration Variables **/
     
     const DEVELOPMENT_ENVIRONMENT = true;
-    
 
     const DB_NAME = 'test';
-    const DB_USER = 'root';
-    const DB_PASSWORD = '1';
-    const DB_HOST = '172.17.0.2.';
+    const DB_USER = 'dangvh';
+    const DB_PASSWORD = 'Vuhaidang157@';
+    const DB_HOST = 'localhost';
