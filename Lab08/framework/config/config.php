f6e90cd792f511cb21f8200bc6f1c32504b9c910
dangzed <dangvh99@gmail.com>
2021-05-21 00:59:40 +0700

    push database review

--- Lab08/framework/config/config.php
+++ Lab08/framework/config/config.php
@@ -6,11 +6,11 @@
     
     
     const DB_NAME = 'framework';
-    const DB_USER = 'root';
-    const DB_PASSWORD = '1';
-    const DB_HOST = '172.17.0.2';
+    const DB_USER = 'dangvh';
+    const DB_PASSWORD = 'Vuhaidang157@';
+    const DB_HOST = 'localhost';
     
-    const BASE_PATH = 'http://localhost/IT4552E-ICT03-K62/Lab08/framework';
+    const BASE_PATH = 'http://dangvh.com/webprog/Lab08/framework';
     
     
     const PAGINATE_LIMIT = '5';
