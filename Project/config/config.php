8169a6580de396165572c19b31e5b032cf84725e
dangzed <dangvh99@gmail.com>
2021-05-22 10:37:37 +0700

    define project structure

---
+++ Project/config/config.php
@@ -0,0 +1,22 @@
+<?php
+    
+    /** Configuration Variables **/
+    
+    const DEVELOPMENT_ENVIRONMENT = true;
+
+// cai nay tu config cua moi nguoi
+    const DB_NAME = 'shop';
+    const DB_USER = 'dangvh';
+    const DB_PASSWORD = 'Vuhaidang157@';
+    const DB_HOST = 'localhost';
+    const BASE_PATH = 'http://dangvh.com/webprog/Lab08/framework';
+
+    // cai nay la de deploy len heroku, tam thoi chua dung den
+//    const DB_NAME = 'heroku_0668a692c23638b';
+//    const DB_USER = 'b422b2ac9415c4';
+//    const DB_PASSWORD = '67f4374d';
+//    const DB_HOST = 'us-cdbr-east-02.cleardb.com';
+
+    
+    
+    const PAGINATE_LIMIT = '5';
