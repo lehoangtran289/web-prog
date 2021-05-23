9d6d7e3b37ba48302543d5b18057d9b8c37528a5
dangzed <dangvh99@gmail.com>
2021-05-23 09:58:00 +0700

    not so buggy

--- dangvhtest/application/controllers/categoriescontroller.php
+++ dangvhtest/application/controllers/categoriescontroller.php
@@ -6,27 +6,22 @@
         }
         
         function view($categoryId = null) {
-            $this->Category->where('parent_id', $categoryId);
-            $this->Category->showHasOne();
-            $this->Category->showHasMany();
-            $subcategories = $this->Category->search();
-            
+
             $this->Category->id = $categoryId;
             $this->Category->showHasOne();
             $this->Category->showHasMany();
             $category = $this->Category->search();
-            
-            $this->set('subcategories', $subcategories);
+
             $this->set('category', $category);
             
         }
         
         
         function index() {
-            $this->Category->orderBy('name', 'ASC');
+            $this->Category->orderBy('brand', 'ASC');
             $this->Category->showHasOne();
             $this->Category->showHasMany();
-            $this->Category->where('parent_id', '0');
+//            $this->Category->where('parent_id', '0');
             $categories = $this->Category->search();
             $this->set('categories', $categories);
         }
