b04bf50cd3e5a04af361f9cb10cb55bec9a11e61
lehoangtran289 <lehoangtran289@gmail.com>
2021-05-26 15:26:04 +0700

    init lab12

--- Lab08/MVC-2/config/config.php
+++ Lab08/MVC-2/config/config.php
@@ -4,7 +4,12 @@
     
     const DEVELOPMENT_ENVIRONMENT = true;
 
+//    const DB_NAME = 'test';
+//    const DB_USER = 'dangvh';
+//    const DB_PASSWORD = 'Vuhaidang157@';
+//    const DB_HOST = 'localhost';
+    
     const DB_NAME = 'test';
-    const DB_USER = 'dangvh';
-    const DB_PASSWORD = 'Vuhaidang157@';
-    const DB_HOST = 'localhost';
+    const DB_USER = 'root';
+    const DB_PASSWORD = '1';
+    const DB_HOST = '172.17.0.2';
