6afdee58e3f446458574dbe986c2717f538bffca
lehoangtran289 <lehoangtran289@gmail.com>
2021-06-02 00:53:47 +0700

    add search products(tobecont)

--- Project/application/controllers/productscontroller.php
+++ Project/application/controllers/productscontroller.php
@@ -46,6 +46,8 @@
                 }
             }
             
+            $brands = performAction('categories', 'findAll', array());
+            $this->set('brands', $brands);
             $this->set('products', $products);
         }
         
@@ -74,6 +76,48 @@
             if ($id != '') {
                 $this->Product->where('id', $id);
                 return $this->Product->search();
+            }
+        }
+        
+        function processSearchReq() {
+            $this->render = 0;
+            error_reporting(0);
+            
+            $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+            
+            if ($contentType === "application/json") {
+                //Receive the RAW post data.
+                $content = trim(file_get_contents("php://input"));
+                $decoded = json_decode($content, true);
+                
+                $db = @mysqli_connect('127.0.0.1', 'root', '1');
+                $sql = "select * from products inner join categories on products.category_id = categories.id where '1'='1'";
+                foreach ($decoded as $key => $value) {
+                    if ($key == 'name') {
+                        $sql .= " and name like " . "'%" . mysqli_real_escape_string($db, $value) . "%'";
+                    }
+                    if ($key == 'brands') {
+                        $sql .= " and brand in (";
+                        foreach ($value as $k => $v) {
+                            if ($k != count($value) - 1) {
+                                $sql .= "'" . mysqli_real_escape_string($db, $v) . "'" . ", ";
+                            } else {
+                                $sql .= "'" . mysqli_real_escape_string($db, $v) . "'";
+                            }
+                        }
+                        $sql .= ") ";
+                    }
+                    if ($key == 'orderBy') {
+                        $sql .= $value == 'lowToHigh' ? "order by price asc" : "order by price desc";
+                    }
+                }
+                
+                global $inflect;
+                $data = $this->Product->custom($sql);
+                $data = array_map(function ($x) use ($inflect) {
+                    return $x[ucfirst($inflect->singularize('products'))];
+                }, $data);
+                echo json_encode($data);
             }
         }
     }
--- Project/application/views/products/index.php
+++ Project/application/views/products/index.php
@@ -1,6 +1,69 @@
-<div><h2>Welcome, this is the home page</h2></div>
+<!-- CSS -->
+<style>
 
-<br/>
+</style>
+
+<!-- JS code -->
+<script type="text/javascript">
+    window.addEventListener('DOMContentLoaded', (event) => {
+        const form = document.getElementById("searchForm");
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+            const formData = new FormData(form);
+            let data = {};
+            for (let [key, value] of formData.entries()) {
+                if (key === 'brands') {
+                    data.hasOwnProperty('brands') ? data['brands'].push(value) : data['brands'] = [value];
+                } else {
+                    if (value) data[key] = value;
+                }
+            }
+            console.log(JSON.stringify(data));
+            fetch("<?php echo BASE_PATH . '/products/processSearchReq' ?>", {
+                method: 'POST',
+                body: JSON.stringify(data),
+                mode: "same-origin",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+                    .then(response => response.text())
+                    .then(data => {
+                        console.log(data);
+                    });
+        })
+    });
+</script>
+
+<!--Product search-->
+<form id="searchForm" method="POST">
+    <!-- Search by name -->
+    <label for="name">Search product name: </label>
+    <input type="text" id="name" name="name" placeholder="Product name"><br><br>
+    
+    <!-- Search by brand name(s) -->
+    <span>Brand: </span>
+    <?php
+        foreach ($brands as $brand) {
+            echo "<input type='checkbox' name='brands' value=" . $brand['Category']['brand'] . ">";
+            echo "<label for=" . $brand['Category']['brand'] . ">" . $brand['Category']['brand'] . "</label>";
+        }
+        echo "<br><br>";
+    ?>
+    
+    <!-- Order by price -->
+    <label for="orderBy">Priority: </label>
+    <select name="orderBy" id="orderBy">
+        <option value="lowToHigh">Low price</option>
+        <option value="highToLow">High Price</option>
+    </select>
+    
+    <br><br>
+    <input type="submit" id="submit" value="Search"/>
+</form>
+
+<!--Products list-->
 <div class="container" style="width:700px;">
     <?php
         foreach ($products as $product) {
