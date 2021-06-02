f979b6222e0fc95562c6292cc8aff4d1ab981ac2
lehoangtran289 <lehoangtran289@gmail.com>
2021-06-02 20:47:26 +0700

    update UI

--- Project/application/views/admin/payments_add.php
+++ Project/application/views/admin/payments_add.php
@@ -12,7 +12,7 @@
 </script>
 
 <br>
-<h2>Add new user</h2>
+<h2>Add new user</h2><br>
 <form name="addPaymentForm" action="<?php echo BASE_PATH . '/admin/payments/add' ?>" method="post" onsubmit="return validateForm()">
     <table>
         <tr>
--- Project/application/views/admin/products_add.php
+++ Project/application/views/admin/products_add.php
@@ -24,7 +24,7 @@
 </script>
 
 <br>
-<h2>Add new user</h2>
+<h2>Add new product</h2><br>
 <form name="addProductForm" action="<?php echo BASE_PATH . '/admin/products/add' ?>" method="post"
       onsubmit="return validateForm()" enctype="multipart/form-data">
     <table>
