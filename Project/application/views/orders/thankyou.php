f2949a4209822a4677cee3d9d322aed9279370ae
Van Hoang <gmlvan99@gmail.com>
2021-06-03 22:51:54 +0700

    save order product

--- Project/application/views/orders/thankyou.php
+++ Project/application/views/orders/thankyou.php
@@ -3,5 +3,6 @@ Thank you for your purchase
 <br>
 <button><a href="<?php echo BASE_PATH?>">Back to Home Page</a></button>
 <?php 
+    echo $_SESSION['insert_id'];
     unset($_SESSION['cart']);
 ?>
