8f3d7c0afb27c005fc65c1bd30130ba62912534d
Van Hoang <gmlvan99@gmail.com>
2021-06-03 23:33:17 +0700

    fix cart ui

--- Project/application/views/orders/thankyou.php
+++ Project/application/views/orders/thankyou.php
@@ -3,6 +3,5 @@ Thank you for your purchase
 <br>
 <button><a href="<?php echo BASE_PATH?>">Back to Home Page</a></button>
 <?php 
-    echo $_SESSION['insert_id'];
     unset($_SESSION['cart']);
 ?>
