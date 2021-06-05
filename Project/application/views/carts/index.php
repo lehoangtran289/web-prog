213a528d52eed7f47b4d01d83dd4e347071bcfe1
tran-haison <haison250199@gmail.com>
2021-06-05 23:29:22 +0700

    Styling cart page

--- Project/application/views/carts/index.php
+++ Project/application/views/carts/index.php
@@ -1,32 +1,32 @@
 <script type='text/javascript'>
-
     function removeItem(id) {
-        const url = "<?php echo BASE_PATH ."/carts/removeFromCart/"?>" + id ;
+        const url = "<?php echo BASE_PATH . "/carts/removeFromCart/" ?>" + id;
         res = fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            displayCart(data);
-        });
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                displayCart(data);
+            });
     }
 
     function decreaseQty(id) {
-        const url = "<?php echo BASE_PATH ."/carts/decreaseItemQty/"?>" + id ;
+        const url = "<?php echo BASE_PATH . "/carts/decreaseItemQty/" ?>" + id;
         res = fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            //console.log(data);
-            displayCart(data);
-        });
+            .then(response => response.json())
+            .then(data => {
+                //console.log(data);
+                displayCart(data);
+            });
     }
+
     function increaseQty(id) {
-        const url = "<?php echo BASE_PATH ."/carts/increaseItemQty/"?>" + id ;
+        const url = "<?php echo BASE_PATH . "/carts/increaseItemQty/" ?>" + id;
         res = fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            displayCart(data);
-        });
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                displayCart(data);
+            });
     }
 
     function displayCart(data) {
@@ -44,7 +44,7 @@
             html += 'Cart Empty';
         } else {
             str = JSON.stringify(data);
-            products = JSON.parse(str);            
+            products = JSON.parse(str);
             html += "<table border='1'>";
             Array.prototype.forEach.call(products, product => {
                 info = JSON.stringify(product);
@@ -54,40 +54,163 @@
                 let segment = '<tr><td>';
                 segment += info.Product.name + ", price is " + info.Product.price + "</td>";
                 segment += "<td><button type=\"button\" onclick=\"decreaseQty(" + info.Product.id + ")\">-</button></td>";
-                segment += "<td>" + info.buy_qty +"</td>";
-                segment += "<td><button type=\"button\" onclick=\"increaseQty(" + info.Product.id+ ")\">+</button></td>";
+                segment += "<td>" + info.buy_qty + "</td>";
+                segment += "<td><button type=\"button\" onclick=\"increaseQty(" + info.Product.id + ")\">+</button></td>";
                 segment += "<td><button type=\"button\" onclick=\"removeItem(" + info.Product.id + ")\">Remove</button></td></tr>";
                 html += segment;
             });
             html += "</table>";
-            html += "<button><a href=" + "<?php echo BASE_PATH . "/orders/index"?>" + ">Buy</a></button>"
+            html += "<button><a href=" + "<?php echo BASE_PATH . "/orders/index" ?>" + ">Buy</a></button>"
         }
         document.getElementById("table").innerHTML = html;
     }
 </script>
-Your Cart:
-<br>
-<div id='table'>
-<?php
+
+<style>
+    .row {
+        justify-content: start;
+    }
+
+    .row h2 {
+        text-transform: uppercase;
+        font-size: 24px;
+        font-weight: 700;
+        transition: all .3s ease 0s;
+    }
+
+    .row p {
+        margin-left: 20px;
+        color: #ff523b;
+        text-transform: uppercase;
+        font-size: 24px;
+        font-weight: 700;
+        transition: all .3s ease 0s;
+    }
+
+    .cart-page {
+        margin: 80px auto;
+    }
+
+    .cart-info {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+    }
+
+    .total-price {
+        display: flex;
+        justify-content: flex-end;
+    }
+
+    .total-price table {
+        border-top: 3px solid #1e1e1eec;
+        width: 100%;
+        max-width: 300px;
+    }
+
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    th {
+        text-align: left;
+        padding: 5px;
+        color: #fff;
+        background: #1e1e1eec;
+        font-weight: normal;
+    }
+
+    td {
+        padding: 10px 5px;
+    }
+
+    td a {
+        color: #ff523b;
+    }
+
+    td img {
+        width: 80px;
+        height: 80px;
+        margin-right: 10px;
+    }
+
+    button {
+        background: #f3f3f3;
+        padding: 5px 8px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    button:hover {
+        background: #ff523b;
+        color: #fff;
+    }
+</style>
+
+<!-- Display cart table -->
+<div class="small-container cart-page">
+    <div class="row row-2">
+        <h2>Your cart</h2>
+    </div>
+    <?php
     session_start();
     if ($cart != 'None') {
-        echo "<table border=\"1\">";
-        foreach ($cart as $item) {
-            $item = json_decode((json_encode(json_decode($item))), true);
-            ?>
-<!--                tat ca thong tin duoc luu trong bien $item nhe son-->
-<!--            <tr><td>--><?php //echo json_encode($item['Product'])?><!--</td>-->
-            <tr><td><?php echo $item['Product']['name'].', price is '.$item['Product']['price']?></td>
-            <td><button type= "button" onclick="decreaseQty(<?php echo $item['Product']['id']?>)">-</button></td>
-            <td><?php echo $item['buy_qty']?></td>
-            <td><button type= "button" onclick="increaseQty(<?php echo $item['Product']['id']?>)">+</button></td>
-            <td><button type= "button" onclick="removeItem(<?php echo $item['Product']['id']?>)">Remove</button></td></tr>
+        $totalPrice = 0;
+    ?>
+        <table>
+            <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Subtotal</th>
+                <th></th>
+            </tr>
             <?php
-        }
-        echo '</table>';
-        echo "<button><a href='" . BASE_PATH . "/orders/index'" . ">Buy</a></button>";
+            foreach ($cart as $item) {
+                $item = json_decode((json_encode(json_decode($item))), true);
+            ?>
+                <tr>
+                    <td>
+                        <div class="cart-info">
+                            <img src="<?php echo BASE_PATH . '/public/images/' . $item['Product']['image'] ?>">
+                            <div>
+                                <p><?php echo $item['Product']['name'] ?></p>
+                            </div>
+                        </div>
+                    </td>
+                    <td>$<?php echo $item['Product']['price'] ?></td>
+                    <td>
+                        <button type="button" onclick="decreaseQty(<?php echo $item['Product']['id'] ?>)">-</button>
+                        <?php echo $item['buy_qty'] ?>
+                        <button type="button" onclick="increaseQty(<?php echo $item['Product']['id'] ?>)">+</button>
+                    </td>
+                    <td>
+                        $
+                        <?php
+                        $subTotal = $item['buy_qty'] * $item['Product']['price'];
+                        $totalPrice += $subTotal;
+                        echo $subTotal;
+                        ?>
+                    </td>
+                    <td><a href="" onclick="removeItem(<?php echo $item['Product']['id'] ?>)">Remove</a></td>
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
+
+        <div class="row" style="margin-top: 40px;">
+            <h2>Total</h2>
+            <p>$<?php echo $totalPrice ?></p>
+            <a href="<?php echo BASE_PATH . '/orders/index'; ?> " class="button" style="margin-left: auto;">Buy</a>
+        </div>
+    <?php
     } else {
-        echo 'Cart Empty';
+        echo '<h4 style="color: red;">Cart Empty</h4>';
     }
-?>
+    ?>
+
+
 </div>
