e988d80da353b1725bf11d1df6e1fdfb48e978b4
dangzed <dangvh99@gmail.com>
2021-06-09 09:57:04 +0700

    nothing

--- Project/application/models/order.php
+++ Project/application/models/order.php
@@ -2,6 +2,5 @@
     
     class Order extends VanillaModel {
         var $hasOne = array('Shipment' => 'Shipment', 'Payment' => 'Payment');
-        var $hasMany = array('Orders_product' => 'Orders_product');
         var $hasManyAndBelongsToMany = array('Product' => 'Product');
     }
